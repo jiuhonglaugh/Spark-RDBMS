3092124e7bb922981a7f0dbaed5e98edd3e39706
久鸿 <34256397+jiuhonglaugh@users.noreply.github.com>
2018-11-29 20:58:49 +0800

    Create SaveMode.java

---
+++ SaveMode.java
@@ -0,0 +1,18 @@
+package com.my.utils.db;
+
+/**
+ * Created by
+ *
+ * @author Mr.zhu
+ * @Date 2018/11/29
+ */
+public enum SaveMode {
+    /**
+     *
+     */
+    Overwrite,
+    /**
+     *
+     */
+    Replace,
+}
